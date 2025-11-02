cc574ca0843781e553769494beeded00f77a4e82
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2025-11-02 12:37:27 -0700

    feat: allow parameter override for showProjectGrid

--- src/curated/projects.ts
+++ src/curated/projects.ts
@@ -1,11 +1,10 @@
-import { WikiJsPost, EnrichedPost } from '../types';
+import { WikiJsPost, EnrichedPost, GridOptions } from '../types';
 import { showGrid } from '../index';
 import { combinePredicates } from '../utils';
 
 type ProjectEnriched = {
   time: Date;
   isBigProject: boolean;
-  filteredTags: string[];
 };
 type ProjectPage = EnrichedPost<ProjectEnriched>;
 
@@ -20,15 +19,10 @@ const enrichProjectPage = (post: WikiJsPost): ProjectPage => {
   const match = /end:(\d\d\d\d-\d\d-\d\d)/g.exec(post.description);
   const time = new Date(match ? match[1] : post.createdAt);
 
-  const filteredTags = post.tags.filter(
-    (tag: string) => tag !== 'big project' && tag !== 'small project'
-  );
-
   return {
     ...post,
     time,
     isBigProject: post.tags.includes('big project'),
-    filteredTags,
   };
 };
 
@@ -37,11 +31,12 @@ const sortProjectsByAdjustedTime = (
   b: EnrichedPost<ProjectEnriched>
 ): number => b.time.getTime() - a.time.getTime();
 
-export const showProjectGrid = async (): Promise<void> => {
+export const showProjectGrid = async (override: GridOptions<ProjectEnriched> = {}): Promise<void> => {
   const isFinishedProject = combinePredicates(isProject, isFinished);
   return showGrid({
     enrichPost: enrichProjectPage,
     filterPost: isFinishedProject,
     sortPost: sortProjectsByAdjustedTime,
+    ...override,
   });
 };
